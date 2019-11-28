f88f6fa075e9304f421c6902ce6fa542970dee9a
TypowyJugol <wojtek.roman254@gmail.com>
2019-11-28 22:27:54 +0100

    lista_6

--- lista_6/SzyfrCezara.py
+++ lista_6/SzyfrCezara.py
@@ -1,22 +1,29 @@
-KLUCZ = 13
+KLUCZ = 15
 
 
 def szyfruj(napis):
     zaszyfrowany = ""
     for i in range(len(napis)):
-        if ord(napis[i]) > 122 - KLUCZ:
-            zaszyfrowany += chr(ord(napis[i]) + KLUCZ - 26)
+        if (((ord(napis[i])>=65) and (ord(napis[i])<=90)) or ((ord(napis[i])>=97) and (ord(napis[i])<=122))):
+            if (ord(napis[i]) > (122 - KLUCZ)):
+                zaszyfrowany += chr(ord(napis[i]) + KLUCZ - 26)
+            else:
+                zaszyfrowany += chr(ord(napis[i]) + KLUCZ)
         else:
-            zaszyfrowany += chr(ord(napis[i]) + KLUCZ)
+            zaszyfrowany += napis[i]
     return zaszyfrowany
-
 
 def deszyfruj(napis):
     zdeszyfrowany = ""
-    KLUCZM = KLUCZ % 26
     for i in range(len(napis)):
-        if (ord(napis[i]) - KLUCZM < 97):
-            zdeszyfrowany += chr(ord(napis[i]) - KLUCZM + 26)
+        if (((ord(napis[i]) >= 65) and (ord(napis[i]) <= 90)) or ((ord(napis[i]) >= 97) and (ord(napis[i]) <= 122))):
+            if ((ord(napis[i]) - KLUCZ) < 97):
+                zdeszyfrowany += chr(ord(napis[i]) - KLUCZ + 26)
+            else:
+                zdeszyfrowany += chr(ord(napis[i]) - KLUCZ)
         else:
-            zdeszyfrowany += chr(ord(napis[i]) - KLUCZM)
+            zdeszyfrowany += napis[i]
     return zdeszyfrowany
+
+
+
---
+++ lista_6/zadanie3.py
@@ -0,0 +1,17 @@
+#znalezione na oeis
+#https://oeis.org/search?q=1%2C11%2C21%2C1211%2C111221&language=polish&go=Wyszukaj
+
+import re
+
+def lookandsay(limit, sequence = 2):
+
+    if limit > 1:
+
+        return lookandsay(limit-1, "".join([str(len(match.group()))+match.group()[0] for matchNum, match in enumerate(re.finditer(r"(\w)\1*", str(sequence)))]))
+
+    else:
+
+        return sequence
+
+n=int(input("Podaj n-ty wyraz ciągu: "))
+print(lookandsay(n))
